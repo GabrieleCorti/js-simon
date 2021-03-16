cb37feac1287b43fa0233fe9f3caa3b0004f7b2f
Gabriele <gabriele.corti.lavoro@gmail.com>
2021-03-16 15:46:41 +0100

    count down attivato

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//genero numero casuale
 function randomNumbBtween(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -6,6 +7,7 @@ function randomNumbBtween(min, max) {
 
 alert("ricordati i seguenti 5 numeri");
 
+//genero i numeri random, li salvo in un array, li faccio vedere al giocatore
 var numeriMemory = [];
     i = 0;
 
@@ -22,4 +24,20 @@ while (i < 5) {
 
 console.log(numeriMemory);
 
-alert("i numerida ricordare sono: " + numeriMemory[0] + " " + numeriMemory[1] + " " + numeriMemory[2] + " " + numeriMemory[3] + " " + numeriMemory[4]);
+var inizioGame = "i numerida ricordare sono: " + numeriMemory[0] + " " + numeriMemory[1] + " " + numeriMemory[2] + " " + numeriMemory[3] + " " + numeriMemory[4]
+alert(inizioGame);
+
+//faccio il conto alla rovescia 
+var time = document.getElementById("count_down");
+var start = 10;
+
+var countDown = setInterval(
+    function(){
+
+        start -= 1;
+        if (start == 0) {
+            clearInterval(countDown);
+        }
+        time.innerHTML = start
+
+}, 1000);
